5f46c7409756b79029293b4b42217cd111c8ca19
Jeremy Shimko <jeremy.shimko@gmail.com>
2018-01-22 11:45:37 -0500

    remove unnecessary DOM event listener

--- content/index.js
+++ content/index.js
@@ -1,26 +1,24 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-  const width = 700;
-  const height = 500;
+const width = 700;
+const height = 500;
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = width;
-  canvas.height = height;
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, width, height);
-  ctx.fill();
-  const imgData = ctx.getImageData(0, 0, width, height);
-  const pix = imgData.data;
-  setInterval(flickering, 30);
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = width;
+canvas.height = height;
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, width, height);
+ctx.fill();
+const imgData = ctx.getImageData(0, 0, width, height);
+const pix = imgData.data;
+setInterval(flickering, 30);
 
-  function flickering() {
-    for (let i = 0; i < pix.length; i += 4) {
-      const color = (Math.random() * 255) + 50;
-      pix[i] = color;
-      pix[i + 1] = color;
-      pix[i + 2] = color;
-    }
-    ctx.putImageData(imgData, 0, 0);
+function flickering() {
+  for (let i = 0; i < pix.length; i += 4) {
+    const color = (Math.random() * 255) + 50;
+    pix[i] = color;
+    pix[i + 1] = color;
+    pix[i + 2] = color;
   }
-});
+  ctx.putImageData(imgData, 0, 0);
+}
